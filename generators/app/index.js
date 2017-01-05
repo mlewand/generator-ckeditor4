81ce685c5458877d9be5ca17ec167991b49e935a
Marek Lewandowski <marek@mlewandowski.com>
2017-01-05 01:48:07 +0100

    Cleanup app/index.

--- generators/app/index.js
+++ generators/app/index.js
@@ -4,14 +4,6 @@ class CKEditor4Generator extends Generator {
 	constructor( args, opts ) {
 		super( args, opts );
 	}
-
-	method1() {
-		console.log( 'method 1 just ran' );
-	}
-
-	method2() {
-		console.log( 'method 2 just ran' );
-	}
 }
 
 module.exports = CKEditor4Generator;
