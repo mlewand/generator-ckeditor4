f487caedb9a843bb217de1b57785b67a20625c53
Marek Lewandowski <marek@mlewandowski.com>
2017-01-07 08:43:09 +0100

    Added tests for issue generator.

---
+++ tests/generators/issue/index.test.js
@@ -0,0 +1,131 @@
+
+let expect = require( 'chai' ).expect,
+	sinon = require( 'sinon' ),
+	path = require( 'path' ),
+	yeomanTest = require( 'yeoman-test' ),
+	proxyquire = require( 'proxyquire' ),
+	openStub = sinon.stub(),
+	testedModulePath = '../../../generators/issue/index';
+
+require( 'chai' ).use( require( 'sinon-chai') );
+require( 'chai' ).use( require( 'chai-as-promised') );
+
+proxyquire( testedModulePath, {
+	open: openStub
+} );
+
+let IssueGenerator = require( testedModulePath );
+
+describe( 'Sample ts', () => {
+	beforeEach( () => {
+		openStub.reset();
+	} );
+
+	describe( 'add', () => {
+		it( 'opens correct URL', () => {
+			return yeomanTest.run( path.join( path.resolve( __dirname ), testedModulePath, '..' ) )
+				.withArguments( [ 'add' ] )
+				.then( function() {
+					expect( openStub ).to.be.calledWith( 'https://dev.ckeditor.com/newticket' );
+					expect( openStub ).to.be.calledOnce;
+				} );
+		} );
+	} );
+
+	describe( 'roadmap', () => {
+		it( 'opens correct URL', () => {
+			return yeomanTest.run( path.join( path.resolve( __dirname ), testedModulePath, '..' ) )
+				.withArguments( [ 'roadmap' ] )
+				.then( function() {
+					expect( openStub ).to.be.calledWith( 'https://dev.ckeditor.com/roadmap' );
+					expect( openStub ).to.be.calledOnce;
+				} );
+		} );
+	} );
+
+	describe( 'open', () => {
+		let stub;
+
+		before( () => {
+			IssueGenerator.prototype._getWorkspace = sinon.stub().returns( {
+				getTicketNumber: sinon.stub().returns( 12345 )
+			} );
+
+			stub = IssueGenerator.prototype._getWorkspace;
+		} );
+
+		after( () => {
+			delete IssueGenerator.prototype._getWorkspace;
+		} );
+
+		it( 'picks version from argument if given', () => {
+			return yeomanTest.run( path.join( path.resolve( __dirname ), testedModulePath, '..' ) )
+				.withArguments( [ 'open', '123' ] )
+				.then( function() {
+					expect( openStub ).to.be.calledWith( 'https://dev.ckeditor.com/ticket/123' );
+					expect( openStub ).to.be.calledOnce;
+				} );
+		} );
+
+		it( 'picks ticket from workspace if not given', () => {
+			return yeomanTest.run( path.join( path.resolve( __dirname ), testedModulePath, '..' ) )
+				.withArguments( [ 'open' ] )
+				.then( function() {
+					expect( openStub ).to.be.calledWith( 'https://dev.ckeditor.com/ticket/12345' );
+					expect( openStub ).to.be.calledOnce;
+				} );
+		} );
+
+		it( 'throws an error if ticket number cant be determined', () => {
+			IssueGenerator.prototype._getWorkspace = sinon.stub().returns( {
+				getTicketNumber: sinon.stub().returns( null )
+			} );
+
+			return expect( yeomanTest.run( path.join( path.resolve( __dirname ), testedModulePath, '..' ) )
+					.withArguments( [ 'open' ] )
+					.then( function() {
+						// Restore original stub.
+						IssueGenerator.prototype._getWorkspace = stub;
+					} )
+				).to.be.rejectedWith( Error, 'Invalid ticket number given (resolved to "null").' );
+		} );
+	} );
+
+	describe( 'milestone', () => {
+		before( () => {
+			IssueGenerator.prototype._getWorkspace = sinon.stub().returns( {
+				getVersion: sinon.stub().returns( '4.4.1' )
+			} );
+		} );
+
+		after( () => {
+			delete IssueGenerator.prototype._getWorkspace;
+		} );
+
+		it( 'picks version from argument if given', () => {
+			return yeomanTest.run( path.join( path.resolve( __dirname ), testedModulePath, '..' ) )
+				.withArguments( [ 'milestone', '4.6.2' ] )
+				.then( function() {
+					expect( openStub ).to.be.calledWith( 'http://dev.ckeditor.com/milestone/CKEditor%204.6.2' );
+					expect( openStub ).to.be.calledOnce;
+				} );
+		} );
+
+		it( 'picks version from workspace if not given', () => {
+			return yeomanTest.run( path.join( path.resolve( __dirname ), testedModulePath, '..' ) )
+				.withArguments( [ 'milestone' ] )
+				.then( function() {
+					expect( openStub ).to.be.calledWith( 'http://dev.ckeditor.com/milestone/CKEditor%204.4.1' );
+					expect( openStub ).to.be.calledOnce;
+				} );
+		} );
+
+		it( 'throws exception if invalid given', () => {
+			return expect(
+					yeomanTest.run( path.join( path.resolve( __dirname ), testedModulePath, '..' ) )
+						.withArguments( [ 'milestone', '4.2' ] )
+				).to.be.rejectedWith( Error, 'Invalid version "4.2" given.' );
+		} );
+	} );
+
+} );
