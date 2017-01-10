ff5e1501010a82344b527504439eeeef6e7e0f5c
Marek Lewandowski <marek@mlewandowski.com>
2017-01-10 01:32:39 +0100

    Added better tests coverage.

--- tests/GeneratorBase.test.js
+++ tests/GeneratorBase.test.js
@@ -1,21 +1,32 @@
-
 let path = require( 'path' ),
-	GeneratorBase = require( '../src/GeneratorBase' );
+	yeomanTest = require( 'yeoman-test' ),
+	WorkspaceStub = sinon.stub(),
+	GeneratorBase;
+
+proxyquire( '../src/GeneratorBase', {
+	'./Workspace': WorkspaceStub
+} );
+
+GeneratorBase = require( '../src/GeneratorBase' );
 
 describe( 'GeneratorBase', () => {
+	let mock;
+
+	beforeEach( () => {
+		mock = {
+			log: sinon.stub(),
+			logVerbose: GeneratorBase.prototype.logVerbose,
+			options: {
+				verbose: false
+			},
+			_getWorkspace: GeneratorBase.prototype._getWorkspace,
+			destinationPath: sinon.stub().returns( '/sample/destinationPath' )
+		};
+
+		WorkspaceStub.reset();
+	} );
 
 	describe( 'logVerbose()', () => {
-		let mock;
-
-		beforeEach( () => {
-			mock = {
-				log: sinon.stub(),
-				logVerbose: GeneratorBase.prototype.logVerbose,
-				options: {
-					verbose: false
-				}
-			};
-		});
 
 		it( 'Logs when verbose option was given', () => {
 			mock.options.verbose = true;
@@ -31,5 +42,40 @@ describe( 'GeneratorBase', () => {
 			expect( mock.log ).not.to.be.called;
 		} );
 	} );
+
+
+	describe( '_getWorkspace()', () => {
+		it( 'creates Workspace instance once', () => {
+			mock._getWorkspace();
+			mock._getWorkspace();
+			mock._getWorkspace();
+
+			expect( WorkspaceStub ).to.be.calledOnce;
+		} );
+
+		it( 'Uses CKEDITOR_DEV_PATH env if dev option is on', () => {
+			let envCkePath = '/custom/env/path/ckeditor-dev',
+				initialEnvValue = process.env.CKEDITOR_DEV_PATH;
+
+			mock.options.dev = true;
+
+			process.env.CKEDITOR_DEV_PATH = envCkePath;
+
+			mock._getWorkspace();
+
+			process.env.CKEDITOR_DEV_PATH = initialEnvValue;
+
+			expect( WorkspaceStub ).to.be.calledOnce;
+			expect( WorkspaceStub ).to.be.calledWithExactly( envCkePath );
+		} );
+
+		it( 'Uses destinationPath', () => {
+			mock._getWorkspace();
+
+			expect( WorkspaceStub ).to.be.calledOnce;
+			expect( WorkspaceStub ).to.be.calledWithExactly( '/sample/destinationPath' );
+		} );
+	} );
+
 
 } );
--- tests/Workspace.test.js
+++ tests/Workspace.test.js
@@ -1,4 +1,3 @@
-
 let path = require( 'path' ),
 	testedModulePathFromRoot = '../src/Workspace',
 	testedModulePath = '../../' + testedModulePathFromRoot,
@@ -6,11 +5,23 @@ let path = require( 'path' ),
 
 describe( 'Workspace', () => {
 
+	let mock;
+
+	beforeEach( () => {
+		mock = new Workspace( '/foo/bar' );
+	} );
+
+	describe( 'constructor', () => {
+		it( 'sets path', () => {
+			mock._path = '/foo/bar';
+		} );
+	} );
+
 	describe( 'getPlugins()', () => {
 		it( 'Returns correct value', () => {
 			let workspaceStub = {
-					_getDirectoryPath: sinon.stub().returns( path.join( __dirname, '_fixtures', 'Workspace', 'getPlugins' ) )
-				};
+				_getDirectoryPath: sinon.stub().returns( path.join( __dirname, '_fixtures', 'Workspace', 'getPlugins' ) )
+			};
 
 			return Workspace.prototype.getPlugins.call( workspaceStub )
 				.then( ( plugins ) => {
@@ -21,14 +32,22 @@ describe( 'Workspace', () => {
 
 		it( 'Returns empty array with nonexisting plugins dir', () => {
 			let workspaceStub = {
-					_getDirectoryPath: sinon.stub().returns( path.join( __dirname, '_fixtures' ) )
-				};
+				_getDirectoryPath: sinon.stub().returns( path.join( __dirname, '_fixtures' ) )
+			};
 
 			return Workspace.prototype.getPlugins.call( workspaceStub )
 				.then( ( plugins ) => {
 					expect( plugins ).to.be.an( 'array' );
 					expect( plugins ).to.be.eql( [] );
 				} );
+		} );
+	} );
+
+	describe( '_getDirectoryPath', () => {
+		it( 'Returns path', () => {
+			mock._path = path.join( 'foo', 'bar', 'baz' );
+
+			expect( mock._getDirectoryPath() ).to.be.equal( mock._path );
 		} );
 	} );
 
