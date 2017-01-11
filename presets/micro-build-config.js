2746a2bf88f372b99f1e17b2d78d8ae438499e45
Marek Lewandowski <marek@mlewandowski.com>
2017-01-11 22:52:06 +0100

    Added a "micro" preset, dedicated for integration test builds.

---
+++ presets/micro-build-config.js
@@ -0,0 +1,47 @@
+/**
+ * @license Copyright (c) 2003-2016, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see http://ckeditor.com/license
+ */
+
+var CKBUILDER_CONFIG = {
+	skin: 'moono-lisa',
+	ignore: [
+		'bender.js',
+		'.bender',
+		'.travis.yml',
+		'bender.ci.js',
+		'yarn.lock',
+		'.yo-rc',
+		'samples',
+		'adapters',
+		'build',
+		'.vscode',
+		'bender-err.log',
+		'bender-out.log',
+		'dev',
+		'.DS_Store',
+		'.editorconfig',
+		'.gitignore',
+		'.gitattributes',
+		'gruntfile.js',
+		'.idea',
+		'.jscsrc',
+		'.jshintignore',
+		'.jshintrc',
+		'less',
+		'.mailmap',
+		'node_modules',
+		'package.json',
+		'README.md',
+		'tests'
+	],
+	plugins: {
+		basicstyles: 1,
+		wysiwygarea: 1
+	},
+	languages : {
+		en: 1,
+		'en-gb': 1,
+		nl: 1
+	}
+};
