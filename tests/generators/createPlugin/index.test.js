5101b73592d17046bbd7c9cc44ecb06303a17136
Marek Lewandowski <marek@mlewandowski.com>
2017-03-14 00:10:35 +0100

    Added test.

--- tests/generators/createPlugin/index.test.js
+++ tests/generators/createPlugin/index.test.js
@@ -2,7 +2,8 @@
 const createPlugin = require( '../../../generators/createPlugin/index' ),
 	compareDirectoryContents = require( '../../_helpers/compareDirectories' ),
 	yeomanTest = require( 'yeoman-test' ),
-	fsp = require( 'fs-promise' );
+	fsp = require( 'fs-promise' ),
+	fse = require( 'fs-extra' );
 
 describe( 'ckeditor4:createPlugin', () => {
 	it( 'creates a basic plugin', () => {
@@ -14,6 +15,33 @@ describe( 'ckeditor4:createPlugin', () => {
 
 				return compareDirectoryContents( path.join( __dirname, '_fixtures', 'expectedSimplePlugin' ),
 					path.join( tmpDir, 'my-plugin' ),
+					{
+						skipEol: false,
+						diff: true
+					} );
+			} );
+	} );
+
+	it( 'creates a basic plugin in a workspace', () => {
+		return yeomanTest.run( path.join( __dirname, '../../../generators/createPlugin' ) )
+			.withArguments( 'my-plugin' )
+			.inTmpDir( function( tmpDir ) {
+				// Let's recreate a dir structure reminding a valid CKE4 installation.
+				let done = this.async();
+
+				return fse.copy(
+						path.join( __dirname, '..', '..', '_fixtures', 'Workspace', '_guessWorkspaceRoot', 'topMostCke' ),
+						tmpDir,
+						done
+					);
+			} )
+			.then( tmpDir => {
+				expect( path.join( tmpDir, 'ckeditor.js' ) ).to.be.a.file();
+				expect( path.join( tmpDir, 'my-plugin' ) ).not.to.be.a.directory();
+				expect( path.join( tmpDir, 'plugins', 'my-plugin' ) ).to.be.a.directory();
+
+				return compareDirectoryContents( path.join( __dirname, '_fixtures', 'expectedSimplePlugin' ),
+					path.join( tmpDir, 'plugins', 'my-plugin' ),
 					{
 						skipEol: false,
 						diff: true
