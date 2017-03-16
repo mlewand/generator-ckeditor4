ee35a3e7bc01b28906cf2d92185b5a8e605f9fa2
Marek Lewandowski <marek@mlewandowski.com>
2017-03-16 01:51:14 +0100

    Added super simple initial unit test.

--- generators/createPlugin/templatesOptional/tests/tests/unit.js
+++ generators/createPlugin/templatesOptional/tests/tests/unit.js
@@ -1,3 +1,9 @@
 /* @bender-ckeditor-plugins: <%= shortName %> */
 
-bender.test( {} );
+bender.editor = true;
+
+bender.test( {
+	'plugin is loaded': function() {
+		assert.isTrue( '<%= shortName %>' in this.editor.plugins, 'Plugin is loaded' );
+	}
+} );
--- tests/generators/createPlugin/_fixtures/expectedTests/unit.js
+++ tests/generators/createPlugin/_fixtures/expectedTests/unit.js
@@ -1,3 +1,9 @@
 /* @bender-ckeditor-plugins: my-plugin */
 
-bender.test( {} );
+bender.editor = true;
+
+bender.test( {
+	'plugin is loaded': function() {
+		assert.isTrue( 'my-plugin' in this.editor.plugins, 'Plugin is loaded' );
+	}
+} );
